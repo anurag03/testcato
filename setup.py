83537695e84c6ef465b40f13c8efb1fae988a208
Anurag <anuragsinha003@gmail.com>
2025-09-20 00:14:53 +0530

    Adding a first working version of pytest test result categorisation tool

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testcato",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A package for categorizing test results.",
+    author="Your Name",
+    author_email="your.email@example.com",
+    license="MIT",
+    install_requires=[],
+)
