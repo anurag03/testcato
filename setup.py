e1bee4e798d719757c855f76728fa6c015305294
Anurag <anuragsinha003@gmail.com>
2025-09-20 23:59:37 +0530

    Versoin

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="testcato",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     description="A package for categorizing test results.",
     author="Anurag",
